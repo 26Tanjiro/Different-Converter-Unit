7d2a2b7e1edb5802726f689f1dda4f19d2e06f38
Jake Penalosa <165337145+26Tanjiro@users.noreply.github.com>
2024-06-18 07:37:49 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -9,17 +9,14 @@ namespace ProojectinOOP
             Console.WriteLine("Different Converter Unit");
             Console.WriteLine("\n*******************************************");
             
-            string[] choose= new string[] { "Time ",
-                                         "Velocity ",
-                                         "Measurement" };
+            string[] choose= new string[] {"Time Converter","Speed Converter ","Length Convert ","Currency Converter","Power Converter","Volume Converter","Mass Converter","Temperature Conveter","Energy Converter", "Area Converter", };
 
             for (int c=0; c< choose.Length; c++)
             {
                 Console.WriteLine($"{ c+1}.{ choose[c]}");
             }
 
-
-            Console.WriteLine("\nChoose Convert you want to Convert:");
+            Console.WriteLine("\nSelect the number you want to use in Converting unit:");
             int convert = Convert.ToInt16(Console.ReadLine());
 
             switch (convert)
@@ -28,18 +25,44 @@ namespace ProojectinOOP
                     ChooseTimeConvert();
                     break;
                 case 2:
-                    ChooseVelocityConvert();
+                    ChooseSpeedConvert();
                     break;
                 case 3:
-                    ChooseMeasurementConvert();
+                    ChooseLengthConvert();
                     break;
+                case 4:
+                    ChooseCurrencyConvert();
+                    break;
+                case 5:
+                    ChoosePowerConvert();
+                    break;
+                case 6:
+                    ChooseVolumeConvert();
+                    break;
+                case 7:
+                    ChooseMassConvert();
+                    break;
+                case 8:
+                    ChooseTemperatureConvert();
+                    break;
+                case 9:
+                    ChooseEnergyConvert();
+                    break;
+                case 10:
+                    ChooseAreaConvert();
+                   break;
+                default:
+                    Console.WriteLine("Invalid Input.......");
+                    return;
+
 
 
 
             }
+            EnteringInputnumbertoConvert();
 
         }
-        public static void ChooseMeasurementConvert()
+        public static void ChooseLengthConvert()
         {
             Console.WriteLine("Choose Measurment Converter:");
             string[] measurementc = new string[] { "m to cm ", 
@@ -82,7 +105,7 @@ namespace ProojectinOOP
             Console.WriteLine("Enter youre choosen converter:");
             int timeconvert = Convert.ToInt16(Console.ReadLine());
         }
-        public static void ChooseVelocityConvert()
+        public static void ChooseSpeedConvert()
         {
             Console.WriteLine("Choose Velocity Converter:");
             string[] velocityc = new string[] { "m to cm ",
@@ -102,7 +125,41 @@ namespace ProojectinOOP
                 Console.WriteLine($"{i + 1}. {velocityc[i]}");
             }
         }
-     
-        
+        public static void ChooseCurrencyConvert()
+        {
+
+        }
+        public static void ChoosePowerConvert()
+        {
+
+        }
+        public static void ChooseVolumeConvert()
+        {
+
+        }
+        public static void ChooseMassConvert()
+        {
+
+        }
+        public static void ChooseTemperatureConvert()
+        {
+
+        }
+        public static void ChooseEnergyConvert()
+        {
+
+        }
+        public static void ChooseAreaConvert()
+        {
+
+        }
+        public static void EnteringInputnumbertoConvert()
+        {
+
+        }
+
+
+
+
     }
 }
